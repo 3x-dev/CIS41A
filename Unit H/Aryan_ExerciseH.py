c9def4382aa2a88a7c480d4c154dc6b2feb11882
3x-dev <aryan.singhal2016@gmail.com>
2024-02-17 19:48:51 -0800

    unit H

--- Unit H/Aryan_ExerciseH.py
+++ Unit H/Aryan_ExerciseH.py
@@ -4,11 +4,72 @@ CIS 41A   Winter 2024
 Unit H, Exercise H
 '''
 
+# Part 1
+class StateData:
+    def __init__(self, name, abbreviation, region, population):
+        self.name = name
+        self.abbreviation = abbreviation
+        self.region = region
+        self.population = population
+    
+    def __str__(self):
+        return self.name
+    
+    def displayState(self):
+        print(f"{self.name} ({self.abbreviation}) is in the {self.region} region and has a population of {self.population}")
 
+# Testing Part 1
+s1 = StateData("California", "CA", "West", 39250000)
+print(s1)  # Calls the __str__ method
+s1.displayState()
+
+# Part 2 
+class StateData2:
+    def __init__(self, name):
+        self.name = name
+    
+    def setRegion(self, region):
+        self.region = region
+    
+    def getRegion(self):
+        return self.region
+
+# Testing Part 2
+s2 = StateData2("California")
+s2.setRegion("West")
+s2.pop = 39250000
+print(s2.name)
+print(s2.getRegion())
+print(s2.region)
+print(s2.pop)
+
+# Part 3
+class StateData3:
+    def __init__(self):
+        self.public = "Public"
+        self._protected = "Protected"
+        self.__private = "Private"
+
+# Testing Part 3
+test = StateData3()
+print(test.public)
+print(test._protected)
+try:
+    print(test.__private)  # Raises an AttributeError
+except AttributeError:
+    print("Traceback error: 'StateData3' object has no attribute '__private'")
 
 
 '''
 Execution results:
 
-
+California
+California (CA) is in the West region and has a population of 39250000
+California
+West
+West
+39250000
+Public
+Protected
+Traceback error: 'StateData3' object has no attribute '__private'
 '''
--- Unit H/Aryan_ProblemH.py
+++ Unit H/Aryan_ProblemH.py
@@ -4,11 +4,55 @@ CIS 41A   Winter 2024
 Unit H, Problem H
 '''
 
+import pickle
 
+# Part One
+class BritCoins:
+    __coinValues = {"pound": 240, "shilling": 12, "penny": 1}
+
+    def __init__(self, **kwargs):
+        self.totalPennies = sum(kwargs.get(coin, 0) * value for coin, value in self.__coinValues.items())
+
+    def __add__(self, other):
+        return BritCoins(penny=self.totalPennies + other.totalPennies)
+
+    def __sub__(self, other):
+        return BritCoins(penny=max(self.totalPennies - other.totalPennies, 0))
+
+    def __str__(self):
+        pounds, remainder = divmod(self.totalPennies, 240)
+        shillings, pennies = divmod(remainder, 12)
+        return f"{pounds} pounds {shillings} shillings {pennies} pennies"
+
+# Testing the BritCoins class
+c1 = BritCoins(pound=4, shilling=24, penny=3)
+c2 = BritCoins(pound=3, shilling=4, penny=5)
+c3 = c1 + c2
+c4 = c1 - c2
+
+print(c1)
+print(c2)
+print(c3)
+print(c4)
+
+# Part Two
+# Pickling c4
+with open('britcoin_c4.pickle', 'wb') as f:
+    pickle.dump(c4, f)
+
+# Unpickling
+with open('britcoin_c4.pickle', 'rb') as f:
+    unpickled_c4 = pickle.load(f)
+
+print(unpickled_c4)
 
 
 '''
 Execution results:
 
-
+5 pounds 4 shillings 3 pennies
+3 pounds 4 shillings 5 pennies
+8 pounds 8 shillings 8 pennies
+1 pounds 19 shillings 10 pennies
+1 pounds 19 shillings 10 pennies
 '''
